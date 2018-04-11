eb798f220e7febdc28557017a5da7c4d1a36b15e
sundara mahalingam <catch.source@gmail.com>
2018-04-11 20:44:43 +0530

    OnClickChange

--- src/App.js
+++ src/App.js
@@ -2,14 +2,47 @@ import React, { Component } from 'react';
 import Photo from './Component/photo'
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      newphoto: false,
+      unphoto_url: "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg",
+    };
+  }
+
+  retunMeUrl = () => {
+    return (this.state.unphoto_url);
+  }
+
+  reqNewImage = (event) => {
+    console.log("event raised");
+
+    if ( this.state.newphoto ) {
+      this.setState({unphoto_url:
+          "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg"});
+
+      this.setState({newphoto: false});      
+    } else {
+      this.setState({unphoto_url:
+          "https://images.pexels.com/photos/428324/pexels-photo-428324.jpeg"});
+
+      this.setState({newphoto: true});      
+    }
+  }
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title"> Welcome to React </h1>
         </header>
         <p>  Hello!!. </p>
-        <Photo />
+        {/* <Photo nurl={this.retunMeUrl()}  /> */ }
+        <Photo nurl={ this.state.unphoto_url } reexp={this.reqNewImage} />
+
       </div>
     );
   }
--- src/Component/photo.js
+++ src/Component/photo.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Photo = () => {
+const Photo = (obj) => {
+
+	console.log(obj);
+
 	return (
 		<div>
-			<img src="https://images.pexels.com/photos/33041/antelope-canyon-lower-canyon-arizona.jpg" 
-			alt="Smiley face" height="200" width="200"/>
+			<img src= {obj.nurl} 
+				alt="Smiley face"
+				onClick={obj.reexp} 
+				height="200" width="200"/>
 		</div>
 	);
 };
